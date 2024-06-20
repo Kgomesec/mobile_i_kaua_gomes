30ba8728275cfa6f9d5fc184c0717c77b1cbaddc
Kauã Gomes <143040333+Kgomesec@users.noreply.github.com>
2024-06-20 07:30:13 -0300

    Copyright

--- Editor-de-imagem/components/EmojiPicker.tsx
+++ Editor-de-imagem/components/EmojiPicker.tsx
@@ -2,7 +2,7 @@ import { Modal, View, Text, Pressable, StyleSheet } from 'react-native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function EmojiPicker({ isVisible, children, onClose } : { isVisible: boolean, children: any, onClose: any }) {
-  return (
+  return (//@author Carlos Eduardo e Kauã Gomes
     <Modal animationType="slide" transparent={true} visible={isVisible}>
       <View style={styles.modalContent}>
         <View style={styles.titleContainer}>
